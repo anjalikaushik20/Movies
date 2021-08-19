a437aaaea8cd07606ad4fe8ca757fcb318f32b54
Anjali Kaushik <anjali.kaushik.209@gmail.com>
2021-08-19 14:23:49 +0530

    image dependencies

---
+++ lib/movie/add-image.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class AddImage extends StatefulWidget {
+  //const AddImage({ Key? key }) : super(key: key);
+
+  @override
+  _AddImageState createState() => _AddImageState();
+}
+
+class _AddImageState extends State<AddImage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
